fa4959c98f722952b19e55669c4caa406d4de48d
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-12 00:45:46 +0000

    01IsOldEnoughToDrink

--- exercises/01-isOldEnoughToDrink/app.js
+++ exercises/01-isOldEnoughToDrink/app.js
@@ -1,3 +1,11 @@
-function isOldEnoughToDrink(){
-    // your code here	
-}
+function isOldEnoughToDrink(age){
+    // your code here
+if(age<21){
+      return false;
+    }
+  else
+    {
+      return true;
+    }}
+var output = isOldEnoughToDrink(22)
+console.log(output);
