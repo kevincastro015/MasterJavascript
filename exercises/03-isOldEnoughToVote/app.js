98d1c171e52124cc9e32a9f470e34e5f84506b0d
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-12 00:53:56 +0000

    3IsOldEnoughToVote

--- exercises/03-isOldEnoughToVote/app.js
+++ exercises/03-isOldEnoughToVote/app.js
@@ -1,3 +1,11 @@
 function isOldEnoughToVote(age) {
   // your code here
-}
+  if(age<18){
+      return false;
+    }
+  else
+    {
+      return true;
+    }}
+var output = isOldEnoughToVote(18)
+console.log(output);
